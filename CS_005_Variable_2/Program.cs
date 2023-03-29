d88fccf016d20c0c0413593b89f2085ae5468548
hki2345 <43468069+hki2345@users.noreply.github.com>
2023-03-29 10:36:19 +0900

    Initialize 1
    KK

---
+++ CS_005_Variable_2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_005_Variable_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            a = 100;
+            Console.WriteLine(a);
+
+            int b = 200;
+            Console.WriteLine(b);
+
+            a = 300;
+            a = 400;
+            a = 500;
+
+            Console.WriteLine(a);
+
+            a = b;
+
+            Console.WriteLine(a);
+
+            int x = 300, y = 400;
+
+            Console.WriteLine("{0}, {1}", x, y);
+        }
+    }
+}
