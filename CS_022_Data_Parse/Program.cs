716a7293a68d3e7e4d0e7e09b1aca694347cf19a
hki2345 <43468069+hki2345@users.noreply.github.com>
2023-03-29 10:46:08 +0900

    Initailize 3
    KK

---
+++ CS_022_Data_Parse/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_022_Data_Parse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string aStr = "100";
+            string bStr = ".123";
+            string cStr = "A";
+
+            int a = int.Parse(aStr);
+            double b = double.Parse(bStr);
+            char c = char.Parse(cStr);
+
+            Console.WriteLine("int.Parse(aStr): {0}", a);
+            Console.WriteLine("double.Parse(bStr): {0}", b);
+            Console.WriteLine("char.Parse(cStr): {0}", c);
+        }
+    }
+}
---
+++ CS_023_Data_Convert/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_022_Data_Parse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string aStr = "100";
+            string bStr = ".123";
+            string cStr = "A";
+
+            int a = Convert.ToInt32(aStr);
+            double b = Convert.ToDouble(bStr);
+            char c = Convert.ToChar(cStr);
+
+            Console.WriteLine("Convert.ToInt32(aStr): {0}", a);
+            Console.WriteLine("Convert.ToDouble(bStr): {0}", b);
+            Console.WriteLine("Convert.ToChar(cStr): {0}", c);
+        }
+    }
+}
