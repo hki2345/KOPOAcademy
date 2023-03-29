2385ae84a8f92c88fe7c3129e5c2dd48c45c4eac
hki2345 <43468069+hki2345@users.noreply.github.com>
2023-03-29 10:54:23 +0900

    Initialize 5
    KK

---
+++ CS_035_Operator_10/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS__35_Operator_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int? a = 100;
+            int? b = null;
+
+            int result;
+
+            result = a ?? 300;
+            Console.WriteLine("a ?? 300 = {0}", result);
+            
+            result = b ?? 300;
+            Console.WriteLine("b ?? 300 = {0}", result);
+        }
+    }
+}
