4dd7a11a31b2a1d0ada724085d14e994e6b1a711
hki2345 <43468069+hki2345@users.noreply.github.com>
2023-03-29 13:35:14 +0900

    Set Function
    KK

---
+++ CSharp_Programming/CS_081_FunctionCheck/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_081_FunctionCheck
+{
+    internal class Program
+    {
+        static int Hap(int a, int b, int c)
+        {
+            return a + b + c;
+        }
+
+        static int Pyung(int a, int b, int c)
+        {
+            return (a + b + c) / 3;
+        }
+
+        static int MaxScore(int a, int b, int c)
+        {
+            if (a > b)
+            {
+                if (a > c)  return a;
+                else        return c;
+            }
+
+            else if (b > c)
+            {
+                if (b > a)  return b;
+                else        return a;
+            }
+
+            return c;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("국어 점수를 입력하세요: ");
+            int tmpX = int.Parse(Console.ReadLine());
+
+            Console.Write("영어 점수를 입력하세요: ");
+            int tmpY = int.Parse(Console.ReadLine());
+
+            Console.Write("수학 점수를 입력하세요: ");
+            int tmpZ = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("합계: {0}", Hap(tmpX, tmpY, tmpZ));
+            Console.WriteLine("평균: {0}", Pyung(tmpX, tmpY, tmpZ));
+            Console.WriteLine("가장 높은 점수: {0}", MaxScore(tmpX, tmpY, tmpZ));
+        }
+    }
+}
