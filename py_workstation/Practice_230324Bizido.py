5a3fddbdf9bbb9765673929a556c5837a8e732c6
hki2345 <43468069+hki2345@users.noreply.github.com>
2023-03-31 14:19:21 +0900

    비지동
    ㅇㅇ

--- py_workstation/Practice_230324Bizido.py
+++ py_workstation/Practice_230324Bizido.py
@@ -1,10 +1,10 @@
 import random
 from math import *
 
-bizidoSize = 100
+bizidoSize = 2000
 bizidoList = list(range(bizidoSize))
 
-gunzipCount = 3
+gunzipCount = 19
 gunzipInitPointList = list(range(gunzipCount))
 
 for i in range(len(bizidoList)):
@@ -18,46 +18,82 @@ def PointDistance(P1, P2):
     return(sqrt((P2[0] - P1[0]) ** 2 + (P2[1] - P1[1]) ** 2))
 
 
-def GunzipToIndex(d1, d2, d3):
-    if(d1 < d2):
-        if(d1 < d3):
-            return 0
-        else:
-            return 2
-
-    else:
-        if(d2 < d3):
-            return 1
-        else:
-            return 2
+def GunzipToIndex(distancelist):
+    return distancelist.index(min(distancelist))
 
 # 대충 잡고 보장
 gunzipPointList = list(range(gunzipCount))
+gunzipAvgPointList = list(range(gunzipCount))
 
 # 2차원 좌표
 for i in range(len(gunzipPointList)):
     gunzipPointList[i] = [range(2)]
 
+# 군집된거 가까운 데로 정리
+def JungLee():
+    for i in range(gunzipCount):
+        gunzipPointList[i].clear()
 
-for i in range(len(bizidoList)):
-    d1 = PointDistance(bizidoList[i], gunzipInitPointList[0])
-    d2 = PointDistance(bizidoList[i], gunzipInitPointList[1])
-    d3 = PointDistance(bizidoList[i], gunzipInitPointList[2])
+    for i in range(len(bizidoList)):
 
-    Index = GunzipToIndex(d1, d2, d3)
-    gunzipPointList[Index].append(bizidoList[i])
+        distanceList = []
+        
+        for j in range(gunzipCount):
+            distanceList.append(PointDistance(bizidoList[i], gunzipInitPointList[j]))
 
+        Index = GunzipToIndex(distanceList)
+        gunzipPointList[Index].append(bizidoList[i])
+        distanceList.clear()
 
-def PyungGyun(list):
+def CheckAvgVsPrev():
+    for i in range(len(gunzipAvgPointList)):
+        if(gunzipInitPointList[i] == gunzipAvgPointList[i]):
+            continue
+        else:
+            for i in range(len(gunzipAvgPointList)):
+                gunzipAvgPointList[i] = gunzipInitPointList[i]
+
+            return False
+        
+    return True
+
+def gunzipReset():
+    for i in range(len(gunzipInitPointList)):
+        gunzipInitPointList[i] = PyungGyun(i, gunzipPointList)
+
+        
+
+# 군집된 거 평균
+def PyungGyun(index, list):
 
     sumX = 0
     sumY = 0
 
-    for i in range(len(list[0])):
-        sumX += list[0, i]
-        sumY += list[0, i]
+    for i in range(len(list[index])):
+        sumX += list[index][i][0]
+        sumY += list[index][i][1]
 
-    return [sumX / len(list[0]), sumY / len(list[0])]
+    if(len(list[index]) != 0):
+        return [(int)(sumX / len(list[index])), (int)(sumY / len(list[index]))]
+    else:
+        return [0, 0]
 
-print(gunzipPointList)
-print(PyungGyun(gunzipPointList))
+while not CheckAvgVsPrev():
+    JungLee()
+    gunzipReset()
+    print(gunzipAvgPointList)
+
+
+
+import matplotlib.pyplot as plt
+plt.figure(figsize=(7, 7))
+
+x = [i[0] for i in gunzipAvgPointList]
+y = [i[1] for i in gunzipAvgPointList]
+for i in range(gunzipCount):
+    X = [j[0] for j in gunzipPointList[i]]
+    Y = [j[1] for j in gunzipPointList[i]]
+    plt.scatter(X, Y, s=60, alpha=0.5)
+
+plt.scatter(x, y, s=100, alpha=1)
+plt.show()
---
+++ py_workstation/Practice_230331.py
@@ -0,0 +1,171 @@
+def sum(a, b):
+    return a + b
+
+def minus(a, b):
+    return a - b
+
+def multi(a, b):
+    return a * b
+
+def sub(a, b):
+    return a / b
+
+
+
+print(sum(5, 3))
+print(minus(5, 3))
+print(multi(5, 3))
+print(sub(5, 3))
+
+
+
+## Filter -> 함수를 통과한 값들만 추려서 배열화
+def func1(n):
+    return n < 3
+
+ls = [1, 2, 3, 4, 5]
+ls2 = filter(func1, ls)
+
+print(list(ls2))
+
+
+## Reduce 각 요소를 연산한 뒤 이전 연산 결과들과 누적해서 반환
+from functools import reduce
+
+num = [1, 2, 3, 4, 5]
+total = reduce(lambda a, b: a + b, num)
+
+print(f"total = {total}")
+
+
+#Z Zip zip -> 하나씩 묶어서 튜플로, map -> 묶어서 리스트로 filter와 차이라면 함수가 들어가지 않음
+
+a = ['a', 'b', 'c', 'd']
+b = [1, 2, 3, 4]
+c = ['A', 'B', 'C']
+
+s = list(zip(a, b, c))
+print(s)
+
+ss = list(map(list, s))
+print(ss)
+
+
+# iterable -> 반복할 수 있는 애이지만 반복하는 애는 아님,  iterator -> 본격적으로 반복하는 애
+# 부가설명
+# Iterable은 List처럼 데이터를 순회할 수 있는 객체입니다.
+# Iterator는 Iterable 객체를 순회하는 객체입니다.
+# Iterable은 iter() 메소들로 Iterator 객체를 생성할 수 있습니다.
+# Iterator는 __next__() 메소드로 Iterable의 데이터에 순차적으로 접근할 수 있습니다.
+# Iterator 객체는 항상 Iterable 객체가 됩니다. 하지만 Iterable 객체는 Iterator 객체가 될 수 있지만 아닐 수도 있습니다.
+
+lst = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+i = iter(lst)
+
+print(next(i))
+print(next(i))
+print(next(i))
+
+
+print(i.__next__())
+print(i.__next__())
+print(i.__next__())
+print(i)
+
+for s in i:
+    print(s)
+
+
+# 제귀함수
+def recur(count):
+    if count == 0:
+        return
+    print(count)
+    count -= 1
+    recur(count)
+
+recur(5)
+          
+
+# 파일 읽기 쓰기
+# f = open('test.txt', 'w')
+# f.write('Python write')
+# f.close()
+
+f = open('test.txt', 'r')
+# s1 = f.read()
+# # s1 = f.read(5)
+# print(s1)
+
+s2 = f.readline()
+print(s2)
+
+# s3 = f.readlines()
+# print(s3)
+# print(s3[1])
+f.close()
+
+
+
+# 1부터 100까지 텍스트파일에 Write 해보기
+f = open('Number.txt', 'w')
+
+for i in range(1, 101):
+    f.write(f'{i}'+'\n')
+
+f.close()
+
+
+f = open('Number.txt', 'r')
+
+ls = f.readlines()
+print(list(map(int, ls)))
+
+f.close()
+
+
+# json -> 파이썬은 json 데이터를 딕셔너리로 받아온다.
+import json
+
+s1 = dict()
+s1['a1'] = 'apple'
+s1['a2'] = 'banana'
+
+s2 = dict()
+s2['a1'] = 'gold'
+s2['a2'] = 'silver'
+
+s3 = dict()
+s3['s1'] = s1
+s3['s2'] = s2
+print(s3)
+
+with open('test.json', 'w', encoding='utf-8') as s:
+    json.dump(s3, s, indent=4)
+
+
+with open('test.json', 'r') as jf:
+    #json_data = f.read()       # <-- str
+    #json_data = f.readline()   # <-- str
+    #json_data = f.readlines()  # <-- list
+    json_data = json.load(jf)   # <-- dict(가장 편한방법)
+
+print(json.dumps(json_data))
+
+# dumps -> s 시에는 string으로 변환이 된다.
+
+print(type(json_data))
+print(type(json.dumps(json_data)))
+print(json_data)
+print(json.dumps(json_data)[5])
+
+
+# 2차원 리스트
+data = [[10, 15],
+        [20, 25],
+        [30, 35]]
+
+print(data[1][1])
+
+data[2][0] = 50
+print(data[2][0])
